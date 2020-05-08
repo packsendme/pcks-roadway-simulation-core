0ac5b4f8757bff14daa10d6ce25e1bf28269295c
ricardomarzochi@gmail.com <r.libera.marzochi@CPX-JGDMHHPGOXX.dir.svc.accenture.com>
2020-05-08 21:38:00 +0200

    NewVersion/0.2.0

--- src/main/java/com/packsendme/microservice/roadway/MicroserviceRoadway_Application.java
+++ src/main/java/com/packsendme/microservice/roadway/MicroserviceRoadway_Application.java
@@ -3,11 +3,10 @@ package com.packsendme.microservice.roadway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableEurekaClient
-@ComponentScan(basePackages = {"com.packsendme.microservice.roadway.component"})
+//@ComponentScan(basePackages = {"com.packsendme.microservice.roadway.component"})
 public class MicroserviceRoadway_Application {
 
 	public static void main(String[] args) {
