49f81cee091b3cfb5e8038026786472c244a1624
ricardomarzochi@gmail.com <r.libera.marzochi@CPX-JGDMHHPGOXX.dir.svc.accenture.com>
2020-05-15 15:05:32 +0200

    NewVersion/0.2.0

--- src/main/java/com/packsendme/microservice/roadway/controller/RoadwayController.java
+++ src/main/java/com/packsendme/microservice/roadway/controller/RoadwayController.java
@@ -5,7 +5,6 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
