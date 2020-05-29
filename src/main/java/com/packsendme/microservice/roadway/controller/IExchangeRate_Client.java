790163b9c0321c33ac49dd28a3338582f93e609b
ricardomarzochi@gmail.com <r.libera.marzochi@CPX-JGDMHHPGOXX.dir.svc.accenture.com>
2020-05-29 19:38:27 +0200

    NewVersion/0.2.0

--- src/main/java/com/packsendme/microservice/roadway/controller/IExchangeRate_Client.java
+++ src/main/java/com/packsendme/microservice/roadway/controller/IExchangeRate_Client.java
@@ -5,11 +5,12 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @FeignClient(name="packsendme-exchange-api")
 public interface IExchangeRate_Client {
 
 	@GetMapping("/api/exchange/{current}")
-	public ResponseEntity<?> getExchange(@Validated  @PathVariable ("current") String current);		
+	public ResponseEntity<?> getExchange(@Validated  @RequestParam ("current") String current);		
 
 }
