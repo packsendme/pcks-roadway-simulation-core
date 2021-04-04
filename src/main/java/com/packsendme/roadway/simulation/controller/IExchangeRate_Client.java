f6aeb3218ff5bb4ebc093cdcccdb6e8bada869f9
ricardomarzochi@gmail.com <r.libera.marzochi@CPX-JGDMHHPGOXX.dir.svc.accenture.com>
2021-04-04 20:20:57 +0200

    ok

--- src/main/java/com/packsendme/roadway/simulation/controller/IExchangeRate_Client.java
+++ src/main/java/com/packsendme/roadway/simulation/controller/IExchangeRate_Client.java
@@ -6,7 +6,7 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name="packsendme-exchange-api")
+@FeignClient(name="pcks-exchange-api")
 public interface IExchangeRate_Client {
 
 	@GetMapping("/api/exchange/rate/{current}")
--- src/main/java/com/packsendme/roadway/simulation/controller/IGoogleAPI_Client.java
+++ src/main/java/com/packsendme/roadway/simulation/controller/IGoogleAPI_Client.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
 
-@FeignClient(name="packsendme-google-api")
+@FeignClient(name="pcks-google-api")
 public interface IGoogleAPI_Client {
 
 	@GetMapping("/distance/{from}/{to}/{measurement}")
